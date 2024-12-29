1a7ea6ae5f86449694dc684244aa691856094748
lucassalles <lucasmssp@gmail.com>
2024-12-28 23:08:00 -0300

    Mudança de imagem, para link da imagem

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ function carregar() {
     time.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
-        image.src = '/img/manha.png'
+        image.src = 'https://github.com/lucassalles-git/hora-do-dia/blob/main/img/manha.png?raw=true'
         document.body.style.background = '#EFD8BB'
         msg.innerHTML = 'Bom Dia!'
     } else if (hora < 18) {
-        image.src = '/img/tarde.png'
+        image.src = 'https://github.com/lucassalles-git/hora-do-dia/blob/main/img/tarde.png?raw=true'
         document.body.style.background = '#A8D4F1'
         msg.innerHTML = 'Boa Tarde!'
     } else {
-        image.src = '/img/noite.png'
+        image.src = 'https://github.com/lucassalles-git/hora-do-dia/blob/main/img/noite.png?raw=true'
         document.body.style.background = '#372D49'
         msg.innerHTML = 'Boa noite!'
     }
